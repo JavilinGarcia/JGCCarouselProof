0e2078e8de126e35c63ec0d5ff34d07c9f4ed518
Javier Garcia Castro <javilin.garcia@gmail.com>
2017-12-25 22:50:05 +0100

    assets

--- JGCCarouselProof/JGCCarouselProof/Extensions/String+JGCExtension.swift
+++ JGCCarouselProof/JGCCarouselProof/Extensions/String+JGCExtension.swift
@@ -13,15 +13,4 @@ extension String {
         return NSStringFromClass(aClass).components(separatedBy: ".").last!
     }
     
-    func substring(from: Int) -> String {
-        return self.substring(from: self.characters.index(self.startIndex, offsetBy: from))
-    }
-    
-    func substring(to: Int) -> String {
-        return self.substring(to: self.characters.index(self.startIndex, offsetBy: to))
-    }
-    
-    var length: Int {
-        return self.characters.count
-    }
 }
